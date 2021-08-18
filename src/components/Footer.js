2c6be01916b475f8a86373f9cf135c9b7c99faaa
adamp117 <87688731+adamp117@users.noreply.github.com>
2021-08-18 12:12:40 -0700

    Created initial files
    Created initial files

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2021 FWD28 Class</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/globals/globals.js
@@ -0,0 +1,3 @@
+// Global Variables
+export const API_KEY = 'Add your API Key here...';
+export const API_TOKEN = 'Add your API Read Access Token (v4 auth) here...';

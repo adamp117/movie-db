eb2a98864ed955806cbb97e7b61e695d1b12d822
bowiemaze <87673359+bowiemaze@users.noreply.github.com>
2021-09-09 20:19:01 -0700

    New logo and footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import logo from '../images/quick-flicks_logo_2.png';
+import logo from '../images/logo_qf.svg';
 import { Link } from 'react-router-dom';
 
 function Footer() {
@@ -7,7 +7,7 @@ function Footer() {
             <div className="footer-info">
 
                 <p>&copy; 2021 quick flicks inc</p>
-                <p><strong>Created By: </strong> Robert Watt, Adam Paquette, and Sara Evans</p>
+                <p><strong>Created By: </strong> Robert Watt | Adam Paquette | Sara Evans</p>
                 <Link to="/"><img src={logo} className="logo" alt="Quick Flicks Logo" /> </Link>
             </div>
         </footer>
